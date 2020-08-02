b0eca3e85cda48ebd95e4149f84a21ad08c5f1e0
Dooniwa <441281580@qq.com>
2020-08-02 09:51:17 +0800

    初始化项目

---
+++ music/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <p>李鑫</p>
+  </div>
+</template>
+
+<style lang="scss">
+  p{
+    width: 300px;
+    height: 300px;
+    background-color: red;
+    transform: translate(50px,50px);
+  }
+</style>
